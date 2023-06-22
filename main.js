9f7c0b865673ee14608a324efe15f004a3386a93
Omid Alikhtl <omidrazi_1989@yahoo.com>
2023-06-21 21:58:25 -0700

    mian.js wass added

---
+++ main.js
@@ -0,0 +1,6 @@
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let equal = document.querySelector(".btn-equal");
+  let clear = document.querySelector(".btn-clear");
+});
