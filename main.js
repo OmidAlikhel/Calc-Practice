76a41d27f89d45a102baef522e9b69a6e5a61255
Omid Alikhtl <omidrazi_1989@yahoo.com>
2023-06-21 23:14:43 -0700

    added two functionality equal and targeting numbers

--- main.js
+++ main.js
@@ -3,4 +3,22 @@
   let buttons = document.querySelectorAll(".btn");
   let equal = document.querySelector(".btn-equal");
   let clear = document.querySelector(".btn-clear");
-});
+
+  // first functionality
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let userInput = e.target.dataset.num;
+      screen.value += userInput;
+    });
+  });
+  // second functionality equal button.
+
+  equal.addEventListener("click", function (e) {
+    if (screen.value === "") {
+      screen.value = "";
+    } else {
+      let answer = eval(screen.value);
+      screen.value = answer;
+    }
+  });
+})();
